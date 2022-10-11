9e6ca08467686e2c66c5aad4bef45623cbd22a6f
Ethan Williams <ethan.williams@inergysolar.com>
2022-10-11 11:51:39 -0600

    Showing as int again

--- ios/Classes/BlufiPlugin.m
+++ ios/Classes/BlufiPlugin.m
@@ -245,7 +245,7 @@
         NSString *customString =  @" ";
         const char* bytes = (const char*)[data bytes];
         for (int i = 0; i < data.length; i++) {
-            customString = [NSString stringWithFormat:@"%@%c%@", customString, bytes[i], @" "];
+            customString = [NSString stringWithFormat:@"%@%d%@", customString, bytes[i], @" "];
         }
         // customString = [customString stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
            [self updateMessage:[self makeJsonWithCommand:@"receive_device_custom_data" data:customString]];
